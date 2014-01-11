cd84d777af9886a15ca405c41114d554af1ce6b1
Paul Dornfeld <pdornfel@gmail.com>
2014-01-11 12:31:20 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NightOut::Application.config.secret_key_base = '2429169fd8ffb7052422eff2763b3d868698e95109d74bfc2ea2bb7f498036cd583c4a452dbab01ee5a0d60e7c5614372ddd033ad251cefae493fc51b4e329a7'
