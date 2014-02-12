658c8d2161c06ef20fd467a72fb0ab5d79d9a690
Paul Dornfeld <pdornfel@gmail.com>
2014-02-12 11:14:37 -0500

    nag controller with create action

---
+++ app/controllers/nags_controller.rb
@@ -0,0 +1,23 @@
+class NagsController < ApplicationController
+
+  def create
+    group = Group.find(params[:group_id])
+    nag = Nag.new(nag_params)
+    nag.group = group
+    nag.user = current_user
+    if nag.save
+      flash[:success] = "You nagged the group"
+      redirect_to group_path(group)
+    else
+      flash[:alert] = "Nag fail"
+      redirect_to group_path(group)
+    end
+  end
+
+  private
+
+  def nag_params
+    params.require(:nag).permit(:body)
+  end
+
+end
